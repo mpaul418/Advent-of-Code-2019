c9eb58ae9f91bc5f29062f4d4bc61940fded675f
Michael McGillicuddy <mpm0418@gmail.com>
2019-12-06 17:13:24 -0500

    not working now

--- day6/day6.c
+++ day6/day6.c
@@ -5,22 +5,71 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+int hash(char* name);
+int intPower(int num, int power);
+
 int main(int argc, char** argv)
 {
-    int lines = 0;
+    int lineCount = 0;
+    int totalPossibleVertices = 0;
+    int i;
+    char line[9];
+    memset(line, '\0', 9);
 
     int fd = open("input.txt", O_RDONLY);
 
-    char line[9];
-    line[8] = '\0';
-
-    while(read(fd, line, 8) == 8)
+    while(read(fd, line, 8) > 0)
     {
-        lines++;
+        lineCount++;
+        totalPossibleVertices += 2;
+
+        printf("%s\n", line);
     }
 
     lseek(fd, 0, SEEK_SET);
 
-    read(fd, line, 8);
-    printf("%s\n", line);
+    printf("%d\n", lineCount);
+
+    for(i = 0; i < lineCount; i++)
+    {
+        char orbitedVertex[4], orbitingVertex[4];
+        orbitedVertex[3] = '\0';
+        orbitingVertex[3] = '\0';
+
+        read(fd, orbitedVertex, 3);
+        read(fd, NULL, 1);
+        read(fd, orbitingVertex, 3);
+        read(fd, NULL, 1);
+
+        printf("%s)%s\n", hash(orbitedVertex), hash(orbitingVertex));
+    }
+}
+
+int hash(char* name)
+{
+    int j, k, result;
+    k = 0;
+    result = 0;
+
+    for(j = strlen(name) - 1; j >= 0; j--)
+    {
+        if(name[j] >= '0' && name[j] <= '9')
+            result += (name[j] - '0') * intPower(100, k);
+        else if(name[j] >= 'A' && name[j] <= 'Z')
+            result += (name[j] - 'A' + 10) * intPower(100, k);
+    }
+
+    return result;
+}
+
+int intPower(int num, int power)
+{
+    int result = 1;
+    while(power > 0)
+    {
+        result *= num;
+        power--;
+    }
+
+    return result;
 }
