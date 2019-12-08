5e40b354d2791aec16329bf62b5d4cebdb486759
Michael McGillicuddy <mpm0418@gmail.com>
2019-12-08 12:54:01 -0500

    Parts 1 and 2 complete

--- day2/day2.py
+++ day2/day2.py
@@ -1,29 +1,43 @@
 import os
 
 def main():
+    os.chdir("Advent of Code 2019")
     os.chdir("day2")
     file = open("input.txt", "r")
     data = file.readline().split(",")
-    data[1] = 12
-    data[2] = 2
+    backup = data.copy()
+
+    print("Part 1 answer: " + str(intcode(data, 12, 2)))
+
+    for i in range(0, 100):
+        for j in range(0, 100):
+            data = backup.copy()
+            if int(intcode(data, i, j)) == 19690720:
+                print("Part 2 answer: " + str(100 * i + j))
+                break
+
+def intcode(theData, noun, verb):
+    theData[1] = noun
+    theData[2] = verb
+
     currentPosition = 0
-    while(currentPosition < len(data)):
-        if int(data[currentPosition]) == 1:
-            firstNum = int(data[int(data[currentPosition + 1])])
-            secondNum = int(data[int(data[currentPosition + 2])])
-            data[int(data[currentPosition + 3])] = str(firstNum + secondNum)
-        elif int(data[currentPosition]) == 2:
-            firstNum = int(data[int(data[currentPosition + 1])])
-            secondNum = int(data[int(data[currentPosition + 2])])
-            data[int(data[currentPosition + 3])] = str(firstNum * secondNum)
-        elif int(data[currentPosition]) == 99:
+    while(currentPosition < len(theData)):
+        if int(theData[currentPosition]) == 1:
+            firstNum = int(theData[int(theData[currentPosition + 1])])
+            secondNum = int(theData[int(theData[currentPosition + 2])])
+            theData[int(theData[currentPosition + 3])] = str(firstNum + secondNum)
+        elif int(theData[currentPosition]) == 2:
+            firstNum = int(theData[int(theData[currentPosition + 1])])
+            secondNum = int(theData[int(theData[currentPosition + 2])])
+            theData[int(theData[currentPosition + 3])] = str(firstNum * secondNum)
+        elif int(theData[currentPosition]) == 99:
             break
         else:
-            print("error happened\n")
+            return -1
 
         currentPosition += 4
 
-    print(str(data[0]))
+    return theData[0]
 
 if __name__== "__main__":
     main()
