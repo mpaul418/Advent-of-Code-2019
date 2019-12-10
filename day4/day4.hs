4113fc7f3beb4944f384f42e3dd03590fb9e4c8b
Michael McGillicuddy <mpm0418@gmail.com>
2019-12-10 02:22:04 -0500

    Part 2 completed

--- day4/day4.hs
+++ day4/day4.hs
@@ -2,10 +2,10 @@ module Day4 where
 
 import Data.Char (digitToInt)
 
-validPassword :: [Int] -> Bool
-validPassword x
-    | neverDecreasing(x) && twoAdjDigits(x) = True
-    | otherwise                             = False
+validPassword :: [Int] -> ([Int] -> Bool) -> Bool
+validPassword x f
+    | neverDecreasing(x) && f x = True
+    | otherwise                 = False
 
 neverDecreasing :: [Int] -> Bool
 neverDecreasing (x1:x2:xs)
@@ -13,15 +13,37 @@ neverDecreasing (x1:x2:xs)
     | otherwise     = neverDecreasing(x2:xs)
 neverDecreasing _   = True
 
-twoAdjDigits :: [Int] -> Bool
-twoAdjDigits (x1:x2:xs)
+twoAdjDigitsP1 :: [Int] -> Bool
+twoAdjDigitsP1 (x1:x2:xs)
     | x1 == x2  = True
-    | otherwise = twoAdjDigits(x2:xs)
-twoAdjDigits _  = False
+    | otherwise = twoAdjDigitsP1(x2:xs)
+twoAdjDigitsP1 _  = False
 
-passwordsInRange :: Int -> Int -> Int
-passwordsInRange start end
-    | start >= end                        = 0
-    | validPassword intAsDigits == True   = 1 + passwordsInRange (start + 1) end
-    | otherwise                           = passwordsInRange (start + 1) end
+twoAdjDigitsP2 :: [Int] -> Bool
+twoAdjDigitsP2 (x1:x2:x3:xs)
+    | x1 == x2 && x2 /= x3  = True
+    | x1 == x2 && x2 == x3  = twoAdjDigitsP2 $ removeDuplicates (xs) x3
+    | otherwise             = twoAdjDigitsP2(x2:x3:xs)
+twoAdjDigitsP2 (x1:x2:[])
+    | x1 == x2              = True
+    | otherwise             = False
+twoAdjDigitsP2 _            = False
+
+removeDuplicates :: [Int] -> Int -> [Int]
+removeDuplicates (x:xs) num
+    | x == num          = removeDuplicates xs num
+    | otherwise         = (x:xs)
+removeDuplicates _ _    = []
+
+passwordsInRange :: Int -> Int -> Int -> Int
+passwordsInRange start end 1
+    | start >= end                                      = 0
+    | validPassword intAsDigits twoAdjDigitsP1 == True  = 1 + passwordsInRange (start + 1) end 1
+    | otherwise                                         = passwordsInRange (start + 1) end 1
     where intAsDigits = map digitToInt (show start)
+passwordsInRange start end 2
+    | start >= end                                      = 0
+    | validPassword intAsDigits twoAdjDigitsP2 == True  = 1 + passwordsInRange (start + 1) end 2
+    | otherwise                                         = passwordsInRange (start + 1) end 2
+    where intAsDigits = map digitToInt (show start)
+passwordsInRange _ _ _                                  = 0
