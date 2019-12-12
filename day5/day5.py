e1a9713f31205a296c3b218498239707f7d87e54
Michael McGillicuddy <mpm0418@gmail.com>
2019-12-12 18:29:10 -0500

    Day 5 Part 1 complete

---
+++ day5/day5.py
@@ -0,0 +1,63 @@
+import os
+
+def main():
+    os.chdir("day5")
+    file = open("input.txt", "r")
+    data = file.readline().split(",")
+
+    intcode(data)
+
+def intcode(theData):
+    currentPosition = 0
+    length = len(theData)
+
+    while(currentPosition < length):
+        instructionNum = 0
+        inputGiven = 5
+        command = int(theData[currentPosition]) % 100
+
+        if command == 99:
+            break
+        elif (command < 1 or command > 4):
+            print("Broke at position " + str(currentPosition))
+            return -1
+        
+        # determine immediate mode or position mode
+        if get_digit(int(theData[currentPosition]), 2) == 0 and (command == 1 or command == 2):
+            value1 = int(theData[int(theData[currentPosition + 1])])
+        else:
+            value1 = int(theData[currentPosition + 1])
+        
+        value2 = 0
+        if currentPosition + 2 < length:
+            if get_digit(int(theData[currentPosition]), 3) == 0 and (command == 1 or command == 2):
+                value2 = int(theData[int(theData[currentPosition + 2])])
+            else:
+                value2 = int(theData[currentPosition + 2])
+
+        value3 = 0
+        if currentPosition + 3 < length:   
+            value3 = int(theData[currentPosition + 3])
+
+        if command == 1:
+            theData[value3] = str(value1 + value2)
+            instructionNum = 4
+        elif command == 2:
+            theData[value3] = str(value1 * value2)
+            instructionNum = 4
+        elif command == 3:
+            theData[value1] = str(inputGiven)
+            instructionNum = 2
+        elif command == 4:
+            print(theData[value1])
+            instructionNum = 2
+
+        currentPosition += instructionNum
+
+    return theData[0]
+
+def get_digit(number, n):
+    return number // 10**n % 10
+
+if __name__== "__main__":
+    main()
